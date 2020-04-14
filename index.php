fd214d263ed4e509c418378972133594766d4cd5
Ontiwellkhongthaw23 <33491778+Ontiwellkhongthaw23@users.noreply.github.com>
2020-04-14 16:26:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+﻿<?php
+  include ('movie_sc_fns.php');
+  include_once('analyticstracking.php');
+  
+  session_start();
+  do_html_header("Welcome to Columbia Movie!");
+
+  echo "<p>Please choose a category:</p>";
+
+  
+  $cat_array = get_categories();
+  
+  display_categories($cat_array);
+
+  display_recommended_movies();
+  
+  if(isset($_SESSION['admin_user'])) {
+    display_button("admin.php", "admin-menu", "Admin Menu");
+  }
+  do_html_footer();
+?>
